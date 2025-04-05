83e50d674c9db80554f827634e10326bc08f5746
DESKTOP-KET66G6\SC Intern <web.squadcell@gmail.com>
2025-04-04 21:48:36 -0300

    made the responsive side bar using sheet

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
+import MobileNav from "@/components/MobileNav";
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -9,9 +11,18 @@ export default function RootLayout({
   const loggedIn = {firstName: 'Adrian', lastName: 'JSM'};
 
   return (
-    <main className="flex h-screen w-full font-in">
-        <Sidebar user={loggedIn} />
-         {children}
-    </main> 
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
   );
 }
