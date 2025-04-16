292276c8b4a9abacf2050616bc4fe68f63aca9c9
DESKTOP-KET66G6\SC Intern <web.squadcell@gmail.com>
2025-04-16 19:02:49 -0300

    Implement auth, appwrite, sentry

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,21 @@
 import HeaderBox from '@/components/HeaderBox';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const Home = () => {
+const Home = async () => {
 
-  const loggedIn  = {firstName : 'Yaqoob', lastName : 'Asfaq', email : 'info@gmail.com'};
+  const loggedIn  = await getLoggedInUser();
 
   return (
     <section className='home'>
       <div className="home-content">
         <header className='home-header'>
-          <HeaderBox 
+          <HeaderBox  
             type='greeting'
             title='welcome'
-            user={loggedIn?.firstName || 'Guest'}
+            user={loggedIn?.name || 'Guest'}
             subtext='Access and manage your account and transactions efficiently'
           />
 
@@ -29,7 +30,7 @@ const Home = () => {
       <RightSidebar
         user={loggedIn}
         transactions={[]}
-        banks={[1]}
+        banks={[]}
       />
       
     </section>
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7bd7aa6695c50b0ee4b143adbf729535@o4509156119347200.ingest.de.sentry.io/4509156120723536",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
